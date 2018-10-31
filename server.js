34320cd5585de65670f2ab6f68f9a79a0d98863a
Sam Watson <Sam.Watson3775@gmail.com>
2018-10-31 02:34:46 -0700

    Change server in file structure

---
+++ server.js
@@ -0,0 +1,31 @@
+// common server for both production and development
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+
+const app = express();
+const server = http.createServer(app);
+
+/* eslint-disable no-console */
+
+// for other settings see
+// http://webpack.github.io/docs/webpack-dev-middleware.html
+
+// app.use(express.static("dist"));
+
+// app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
+
+// set up env vars first
+// require('../config/passport')(passport);
+
+// app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+server.listen(process.env.PORT || 8080);
+/* eslint-disable no-console */
