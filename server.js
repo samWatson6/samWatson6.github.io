922a503069a11d92ad6034d2f8d9c945e1a65b26
Sam Watson <Sam.Watson3775@gmail.com>
2018-10-31 02:35:58 -0700

    This one is it

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 // set up env vars first
 // require('../config/passport')(passport);
 
-// app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static( "dist")
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
