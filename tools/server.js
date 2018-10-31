f7d17ddd57d6576c2aa4011b7b27aba4ce261b47
Sam Watson <Sam.Watson3775@gmail.com>
2018-10-31 02:25:17 -0700

    add parens

--- tools/server.js
+++ tools/server.js
@@ -19,10 +19,10 @@ const server = http.createServer(app);
 // set up env vars first
 // require('../config/passport')(passport);
 
-app.use(express.static(path.join(__dirname, "dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "dist/index.html");
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
